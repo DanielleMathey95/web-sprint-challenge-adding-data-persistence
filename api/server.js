f6a17cf9619ea6f8856fcd24d1acf18033cd9938
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-26 14:12:23 -0700

    dependencies installed and server.js complete

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
 // build your server here and require it from index.js
+require('dotenv').config()
+
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`)
+})
